a433bc012b9c9b1ae110b0ff0f6d2cbd6b199812
Jonas Xavier <jonas.agx@gmail.com>
2022-09-17 12:39:50 -0700

    format
    Signed-off-by: Jonas Xavier <jonas.agx@gmail.com>

--- main.c
+++ main.c
@@ -21,16 +21,15 @@ media = (nota1+nota2+nota3+nota4)/4;
 printf("\n\nSua nota final é: %.2f", media);
 
 if (media >= 7.0) {                                           
-printf ("\n\nAprovado por média"); 
+  printf ("\n\nAprovado por média");
 }
     
 else {
-printf ("\n\nReprovado");
-printf ("\n\nDigite seu nome:");
-scanf ("%s", &nome);
-printf("\n\n%s as provas finais ocorrerão no dia 12/12", &nome);
-}
+  printf ("\n\nReprovado");
+  printf ("\n\nDigite seu nome:");
+  scanf ("%s", &nome);
+  printf("\n\n%s as provas finais ocorrerão no dia 12/12", &nome);
+ }
 
   return 0;
-
 }
